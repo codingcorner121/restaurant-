399c2e001482431b53d7ff51cd02d63424bc94d9
umashankar sharma <codingcorner@gmail.com>
2024-06-21 00:28:00 +0530

    header image rename again

--- src/Componests/Header/Header.jsx
+++ src/Componests/Header/Header.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import './Header.css'
+import { assets } from '../../assets/assets';
 
 
 function Header(){
     return(
         <div className="header">
+            
+            
             <div className='header-content'>
                 <h2>Order your favorite food here</h2>
                 <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary experties. Our mission is to satisfy your cravings and elevate your dining experience, and delicious meal at a time.</p>
                 <button>View Menu</button>
             </div>
+
+            
 
         </div>
     )
